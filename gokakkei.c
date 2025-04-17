95564b6d54c23b303340b1c52be966e12af68bc5
CatnyapC <lynn1306962652@gmail.com>
2025-04-17 14:11:27 +0900

    rotate

--- gokakkei.c
+++ gokakkei.c
@@ -1,9 +1,15 @@
+
 #include <GLUT/glut.h>
+
+float angle = 0.0f;
 
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 	gluLookAt(0, 0, 3, 0, 0, 0, 0, 1, 0);
+
+	glPushMatrix();
+	glRotatef(angle, 0.0f, 1.0f, 0.0f);
 
 	glColor3f(1.0, 0.0, 0.0);
 	glBegin(GL_POLYGON);
@@ -14,7 +20,14 @@ void display(void) {
 	glVertex3f(-0.5, 0.5, 0.0);
 	glEnd();
 
+	glPopMatrix();
 	glFlush();
+}
+
+void idle(void) {
+	angle += 0.2f;
+	if (angle > 360.0f) angle -= 360.0f;
+	glutPostRedisplay();
 }
 
 void init(void) {
@@ -41,7 +54,7 @@ int main(int argc, char **argv) {
 	init();
 	glutDisplayFunc(display);
 	glutKeyboardFunc(key);
+	glutIdleFunc(idle);
 	glutMainLoop();
 	return 0;
 }
-
